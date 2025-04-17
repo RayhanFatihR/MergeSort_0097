99daa6757595bbc64d975b77698920e11d756c34
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-04-17 18:14:59 +0700

    MergeSort T2

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -3,3 +3,26 @@ using namespace std;
 
 int arr[20], B[20]; // ArrayUtama dan ArrayBantu
 int n;
+
+void input()
+{
+    while(true)
+    {
+        cout << "Masukkan Panjang elemen array: ";
+        cin >> n;
+        if (n <= 20)
+        break;
+        cout << "\nMaksimal panjang array adalah 20\n" << endl;
+    }
+
+
+
+
+
+}
+
+void mergeSort(int low, int high){
+    if(low >= high){
+        return;
+    }
+}
