9daed85d72f364a6db6b2d6d78609eb4fdec96b7
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-04-17 18:46:46 +0700

    MergeSort T11

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -36,23 +36,35 @@ void mergeSort(int low, int high){
     j = mid + 1; //step 4b
     k = low; //step 4c
 
-    while(i <= mid && j <= high){ //step 4d
-        if(arr[i] <= arr[j]){ //step 4.d.i
-            B[k] = arr[i]; //step 4.d.i.1
-            i++; //step 4.d.i.2
+    while(i <= mid && j <= high){   //step 4d
+        if(arr[i] <= arr[j]){       //step 4.d.i
+            B[k] = arr[i];          //step 4.d.i.1
+            i++;                    //step 4.d.i.2
         }
         else{
-            B[k] = arr[j]; //step 4.d.i.3
-            j++; //step 4.d.i.4
+            B[k] = arr[j];  //step 4.d.i.3
+            j++;            //step 4.d.i.4
         }
-        k++; //step 4.d.ii
+        k++;                //step 4.d.ii
     }
 
     while(j <= high) //step 4e
     {
-        B[k] = arr[j]; //step 4e.i
-        j++; //step 4e.ii
-        k++; //step 4e.iii
+        B[k] = arr[j];  //step 4e.i
+        j++;            //step 4e.ii
+        k++;            //step 4e.iii
+    }
+
+    while(i <= mid)
+    {
+        B[k] = arr[i];   //step 4f.i
+        i++;             //step 4f.ii
+        k++;             //step 4f.iii
+    }
+
+    for( int x = low; x <= high; x++ )  //step 5
+    {
+        arr[x] = B[x];                  
     }
 
 
